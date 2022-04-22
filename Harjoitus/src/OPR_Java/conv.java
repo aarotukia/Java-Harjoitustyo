1b8be6260d4240ae84f76fc126dbb6f5b6854b4b
Mika <mika.lukkarinen@edu.turkuamk.fi>
2022-04-22 19:06:57 +0300

    Päivitystä
    Lisää yksikon muunnoksia lisätty

--- Harjoitus/src/OPR_Java/conv.java
+++ Harjoitus/src/OPR_Java/conv.java
@@ -18,5 +18,67 @@ public class conv {
     final double mm_to_acre = 0;
     final double mm_to_square_mile = 0;
     //CEMTIMETERS
-
+    final double cm_to_mm = 0.1;
+    final double cm_to_dm = 0.1;
+    final double cm_to_m = 0.1;
+    final double cm_to_dam= 0.1;
+    final double cm_to_hm = 0.1;
+    final double cm_to_km = 0.1;
+    final double cm_to_inces = 0.1;
+    final double cm_to_foot = 0.1;
+    final double cm_to_yard = 0.1;
+    final double cm_to_mile = 0.1;
+    final double cm_to_square_meter = 0.1;
+    final double cm_to_are = 0.1;
+    final double cm_to_hectare = 0.1;
+    final double cm_to_acre = 0.1;
+    final double cm_to_square_mile = 0.1;
+    //DECIMETER
+    final double dm_to_mm = 0.1;
+    final double dm_to_cm = 0.1;
+    final double dm_to_m = 0.1;
+    final double dm_to_dam= 0.1;
+    final double dm_to_hm = 0.1;
+    final double dm_to_km = 0.1;
+    final double dm_to_inces = 0.1;
+    final double dm_to_foot = 0.1;
+    final double dm_to_yard = 0.1;
+    final double dm_to_mile = 0.1;
+    final double dm_to_square_meter = 0.1;
+    final double dm_to_are = 0.1;
+    final double dm_to_hectare = 0.1;
+    final double dm_to_acre = 0.1;
+    final double dm_to_square_mile = 0.1;
+    //METER
+    final double m_to_mm = 0.1;
+    final double m_to_cm = 0.1;
+    final double m_to_dm = 0.1;
+    final double m_to_dam= 0.1;
+    final double m_to_hm = 0.1;
+    final double m_to_km = 0.1;
+    final double m_to_inces = 0.1;
+    final double m_to_foot = 0.1;
+    final double m_to_yard = 0.1;
+    final double m_to_mile = 0.1;
+    final double m_to_square_meter = 0.1;
+    final double m_to_are = 0.1;
+    final double m_to_hectare = 0.1;
+    final double m_to_acre = 0.1;
+    final double m_to_square_mile = 0.1;
+    //DECAMETER
+    final double dam_to_mm = 0.1;
+    final double dam_to_cm = 0.1;
+    final double dam_to_dm = 0.1;
+    final double dam_to_m= 0.1;
+    final double dam_to_hm = 0.1;
+    final double dam_to_km = 0.1;
+    final double dam_to_inces = 0.1;
+    final double dam_to_foot = 0.1;
+    final double dam_to_yard = 0.1;
+    final double dam_to_mile = 0.1;
+    final double dam_to_square_meter = 0.1;
+    final double dam_to_are = 0.1;
+    final double dam_to_hectare = 0.1;
+    final double dam_to_acre = 0.1;
+    final double dam_to_square_mile = 0.1;
 }
