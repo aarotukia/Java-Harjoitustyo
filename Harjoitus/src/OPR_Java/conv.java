ca95448cbdc7a609d541234001a38884d219e108
Mika <mika.lukkarinen@edu.turkuamk.fi>
2022-04-22 19:39:10 +0300

    Muokkaus valmis

--- Harjoitus/src/OPR_Java/conv.java
+++ Harjoitus/src/OPR_Java/conv.java
@@ -11,12 +11,59 @@ public class conv {
     final double mm_to_in = 0.039;
     final double mm_to_ft = 0.0032;
     final double mm_to_yd = 0.00109;
-    final double mm_to_mile = 6.213711922E-7;
-    final double mm_to_square_meter = 1000;
+    final double mm_to_mi = 6.213711922E-7;
+    final double mm_to_square_meter = 0;
     final double mm_to_are = 0;
     final double mm_to_hectare = 0;
     final double mm_to_acre = 0;
-    final double mm_to_square_mile = 0;
+    final double mm_to_square_mi = 0;
+    //GETTER LAUSEET ALHAALLA
+    public double getMm_to_cm() {
+        return this.mm_to_cm;
+    }
+    public double getMm_to_dm() {
+        return this.mm_to_dm;
+    }
+    public double getMm_to_m() {
+        return this.mm_to_m;
+    }
+    public double getMm_to_dam() {
+        return this.mm_to_dam;
+    }
+    public double getMm_to_hm() {
+        return this.mm_to_hm;
+    }
+    public double getMm_to_km() {
+        return this.mm_to_km;
+    }
+    public double getMm_to_in() {
+        return this.mm_to_in;
+    }
+    public double getMm_to_ft() {
+        return this.mm_to_ft;
+    }
+    public double getMm_to_yd() {
+        return this.mm_to_yd;
+    }
+    public double getMm_to_mi() {
+        return this.mm_to_mi;
+    }
+    public double getMm_to_square_meter() {
+        return this.mm_to_square_meter;
+    }
+    public double getMm_to_are() {
+        return this.mm_to_are;
+    }
+    public double getMm_to_hectare() {
+        return this.mm_to_hectare;
+    }
+    public double getMm_to_acre() {
+        return this.mm_to_acre;
+    }
+    public double getMm_to_square_mi() {
+        return this.mm_to_square_mi;
+    }
+
     //CEMTIMETERS
     final double cm_to_mm = 0.1;
     final double cm_to_dm = 0.1;
@@ -27,12 +74,12 @@ public class conv {
     final double cm_to_in = 0.1;
     final double cm_to_ft = 0.1;
     final double cm_to_yd = 0.1;
-    final double cm_to_mile = 0.1;
+    final double cm_to_mi = 0.1;
     final double cm_to_square_meter = 0.1;
     final double cm_to_are = 0.1;
     final double cm_to_hectare = 0.1;
     final double cm_to_acre = 0.1;
-    final double cm_to_square_mile = 0.1;
+    final double cm_to_square_mi = 0.1;
     //DECIMETER
     final double dm_to_mm = 0.1;
     final double dm_to_cm = 0.1;
@@ -43,12 +90,12 @@ public class conv {
     final double dm_to_in = 0.1;
     final double dm_to_ft = 0.1;
     final double dm_to_yd = 0.1;
-    final double dm_to_mile = 0.1;
+    final double dm_to_mi = 0.1;
     final double dm_to_square_meter = 0.1;
     final double dm_to_are = 0.1;
     final double dm_to_hectare = 0.1;
     final double dm_to_acre = 0.1;
-    final double dm_to_square_mile = 0.1;
+    final double dm_to_square_mi = 0.1;
     //METER
     final double m_to_mm = 0.1;
     final double m_to_cm = 0.1;
@@ -59,12 +106,12 @@ public class conv {
     final double m_to_in = 0.1;
     final double m_to_ft = 0.1;
     final double m_to_yd = 0.1;
-    final double m_to_mile = 0.1;
+    final double m_to_mi = 0.1;
     final double m_to_square_meter = 0.1;
     final double m_to_are = 0.1;
     final double m_to_hectare = 0.1;
     final double m_to_acre = 0.1;
-    final double m_to_square_mile = 0.1;
+    final double m_to_square_mi = 0.1;
     //DECAMETER
     final double dam_to_mm = 0.1;
     final double dam_to_cm = 0.1;
@@ -75,12 +122,12 @@ public class conv {
     final double dam_to_in = 0.1;
     final double dam_to_ft = 0.1;
     final double dam_to_yd = 0.1;
-    final double dam_to_mile = 0.1;
+    final double dam_to_mi = 0.1;
     final double dam_to_square_meter = 0.1;
     final double dam_to_are = 0.1;
     final double dam_to_hectare = 0.1;
     final double dam_to_acre = 0.1;
-    final double dam_to_square_mile = 0.1;
+    final double dam_to_square_mi = 0.1;
     //HECTOMETER
     final double hm_to_mm = 0.1;
     final double hm_to_cm = 0.1;
@@ -91,12 +138,12 @@ public class conv {
     final double hm_to_in = 0.1;
     final double hm_to_ft = 0.1;
     final double hm_to_yd = 0.1;
-    final double hm_to_mile = 0.1;
+    final double hm_to_mi = 0.1;
     final double hm_to_square_meter = 0.1;
     final double hm_to_are = 0.1;
     final double hm_to_hectare = 0.1;
     final double hm_to_acre = 0.1;
-    final double hm_to_square_mile = 0.1;
+    final double hm_to_square_mi = 0.1;
     //KILOMETERS
     final double km_to_mm = 0.1;
     final double km_to_cm = 0.1;
@@ -107,12 +154,12 @@ public class conv {
     final double km_to_in = 0.1;
     final double km_to_ft = 0.1;
     final double km_to_yd = 0.1;
-    final double km_to_mile = 0.1;
+    final double km_to_mi = 0.1;
     final double km_to_square_meter = 0.1;
     final double km_to_are = 0.1;
     final double km_to_hectare = 0.1;
     final double km_to_acre = 0.1;
-    final double km_to_square_mile = 0.1;
+    final double km_to_square_mi = 0.1;
     //INCES
     final double in_to_mm = 0.1;
     final double in_to_cm = 0.1;
@@ -123,12 +170,12 @@ public class conv {
     final double in_to_in = 0.1;
     final double in_to_ft = 0.1;
     final double in_to_yd = 0.1;
-    final double in_to_mile = 0.1;
+    final double in_to_mi = 0.1;
     final double in_square_meter = 0.1;
     final double in_to_are = 0.1;
     final double in_to_hectare = 0.1;
     final double in_to_acre = 0.1;
-    final double in_to_square_mile = 0.1;
+    final double in_to_square_mi = 0.1;
     //FOOT
     final double ft_to_mm = 0.1;
     final double ft_to_cm = 0.1;
@@ -139,12 +186,12 @@ public class conv {
     final double ft_to_im = 0.1;
     final double ft_to_km = 0.1;
     final double ft_to_yd = 0.1;
-    final double ft_to_mile = 0.1;
+    final double ft_to_mi = 0.1;
     final double ft_square_meter = 0.1;
     final double ft_to_are = 0.1;
     final double ft_to_hectare = 0.1;
     final double ft_to_acre = 0.1;
-    final double ft_to_square_mile = 0.1;
+    final double ft_to_square_mi = 0.1;
     //YARD
     final double yd_to_mm = 0.1;
     final double yd_to_cm = 0.1;
@@ -155,13 +202,13 @@ public class conv {
     final double yd_to_im = 0.1;
     final double yd_to_km = 0.1;
     final double yd_to_ft = 0.1;
-    final double yd_to_mile = 0.1;
+    final double yd_to_mi = 0.1;
     final double yd_square_meter = 0.1;
     final double yd_to_are = 0.1;
     final double yd_to_hectare = 0.1;
     final double yd_to_acre = 0.1;
-    final double yd_to_square_mile = 0.1;
-     //MILE
+    final double yd_to_square_mi = 0.1;
+     //Mile
     final double mi_to_mm = 0.1;
     final double mi_to_cm = 0.1;
     final double mi_to_dm = 0.1;
