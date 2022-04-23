774b324f6fe8586f89b31026d57c4988ded33243
Mika <mika.lukkarinen@edu.turkuamk.fi>
2022-04-23 15:07:18 +0300

    regionit lisätty
    auttaa editorissa collapsaamaan coodia hellpolukuisuuden tukemiseksi

--- Harjoitus/src/OPR_Java/conv.java
+++ Harjoitus/src/OPR_Java/conv.java
@@ -1,7 +1,7 @@
 package OPR_Java;
 
 public class conv {
-    //MILIMETERS
+    //region MILIMETERS
     final double mm_to_cm = 0.1;
     final double mm_to_dm = 0.01;
     final double mm_to_m = 0.001;
@@ -17,54 +17,9 @@ public class conv {
     final double mm_to_hectare = 0;
     final double mm_to_acre = 0;
     final double mm_to_square_mi = 0;
-    //GETTER LAUSEET ALHAALLA
-    public double getMm_to_cm() {
-        return this.mm_to_cm;
-    }
-    public double getMm_to_dm() {
-        return this.mm_to_dm;
-    }
-    public double getMm_to_m() {
-        return this.mm_to_m;
-    }
-    public double getMm_to_dam() {
-        return this.mm_to_dam;
-    }
-    public double getMm_to_hm() {
-        return this.mm_to_hm;
-    }
-    public double getMm_to_km() {
-        return this.mm_to_km;
-    }
-    public double getMm_to_in() {
-        return this.mm_to_in;
-    }
-    public double getMm_to_ft() {
-        return this.mm_to_ft;
-    }
-    public double getMm_to_yd() {
-        return this.mm_to_yd;
-    }
-    public double getMm_to_mi() {
-        return this.mm_to_mi;
-    }
-    public double getMm_to_square_meter() {
-        return this.mm_to_square_meter;
-    }
-    public double getMm_to_are() {
-        return this.mm_to_are;
-    }
-    public double getMm_to_hectare() {
-        return this.mm_to_hectare;
-    }
-    public double getMm_to_acre() {
-        return this.mm_to_acre;
-    }
-    public double getMm_to_square_mi() {
-        return this.mm_to_square_mi;
-    }
+    //endregion
 
-    //CEMTIMETERS
+    //region CEMTIMETERS
     final double cm_to_mm = 10;
     final double cm_to_dm = 0.1;
     final double cm_to_m = 0.01;
@@ -80,7 +35,8 @@ public class conv {
     final double cm_to_hectare = 0.1;
     final double cm_to_acre = 0.1;
     final double cm_to_square_mi = 0.1;
-    //DECIMETER
+    //endregion
+    //region DECIMETER
     final double dm_to_mm = 100;
     final double dm_to_cm = 10;
     final double dm_to_m = 0.1;
@@ -96,7 +52,8 @@ public class conv {
     final double dm_to_hectare = 0.1;
     final double dm_to_acre = 0.1;
     final double dm_to_square_mi = 0.1;
-    //METER
+    //endregion
+    //region METER
     final double m_to_mm = 1000;
     final double m_to_cm = 100;
     final double m_to_dm = 10;
@@ -112,7 +69,8 @@ public class conv {
     final double m_to_hectare = 0.1;
     final double m_to_acre = 0.1;
     final double m_to_square_mi = 0.1;
-    //DECAMETER
+    //endregion
+    //region DECAMETER
     final double dam_to_mm = 10000;
     final double dam_to_cm = 1000;
     final double dam_to_dm = 100;
@@ -128,7 +86,8 @@ public class conv {
     final double dam_to_hectare = 0.1;
     final double dam_to_acre = 0.1;
     final double dam_to_square_mi = 0.1;
-    //HECTOMETER
+    //regionend
+    //region HECTOMETER
     final double hm_to_mm = 0.1;
     final double hm_to_cm = 0.1;
     final double hm_to_dm = 0.1;
@@ -144,7 +103,8 @@ public class conv {
     final double hm_to_hectare = 0.1;
     final double hm_to_acre = 0.1;
     final double hm_to_square_mi = 0.1;
-    //KILOMETERS
+    //endregion
+    //region KILOMETERS
     final double km_to_mm = 0.1;
     final double km_to_cm = 0.1;
     final double km_to_dm = 0.1;
@@ -160,7 +120,8 @@ public class conv {
     final double km_to_hectare = 0.1;
     final double km_to_acre = 0.1;
     final double km_to_square_mi = 0.1;
-    //INCES
+    //endregion
+    //region INCES
     final double in_to_mm = 0.1;
     final double in_to_cm = 0.1;
     final double in_to_dm = 0.1;
@@ -176,7 +137,8 @@ public class conv {
     final double in_to_hectare = 0.1;
     final double in_to_acre = 0.1;
     final double in_to_square_mi = 0.1;
-    //FOOT
+    //endregion
+    //region FOOT
     final double ft_to_mm = 0.1;
     final double ft_to_cm = 0.1;
     final double ft_to_dm = 0.1;
@@ -192,7 +154,8 @@ public class conv {
     final double ft_to_hectare = 0.1;
     final double ft_to_acre = 0.1;
     final double ft_to_square_mi = 0.1;
-    //YARD
+    //endregion
+    //region YARD
     final double yd_to_mm = 0.1;
     final double yd_to_cm = 0.1;
     final double yd_to_dm = 0.1;
@@ -208,7 +171,8 @@ public class conv {
     final double yd_to_hectare = 0.1;
     final double yd_to_acre = 0.1;
     final double yd_to_square_mi = 0.1;
-     //Mile
+    //endregion
+     //region Mile
     final double mi_to_mm = 0.1;
     final double mi_to_cm = 0.1;
     final double mi_to_dm = 0.1;
@@ -224,5 +188,833 @@ public class conv {
     final double mi_to_hectare = 0.1;
     final double mi_to_acre = 0.1;
     final double mi_to_square_mi = 0.1;
-    
+    //endregion
+
+//region getter
+
+    public double getMm_to_cm() {
+        return this.mm_to_cm;
+    }
+
+
+    public double getMm_to_dm() {
+        return this.mm_to_dm;
+    }
+
+
+    public double getMm_to_m() {
+        return this.mm_to_m;
+    }
+
+
+    public double getMm_to_dam() {
+        return this.mm_to_dam;
+    }
+
+
+    public double getMm_to_hm() {
+        return this.mm_to_hm;
+    }
+
+
+    public double getMm_to_km() {
+        return this.mm_to_km;
+    }
+
+
+    public double getMm_to_in() {
+        return this.mm_to_in;
+    }
+
+
+    public double getMm_to_ft() {
+        return this.mm_to_ft;
+    }
+
+
+    public double getMm_to_yd() {
+        return this.mm_to_yd;
+    }
+
+
+    public double getMm_to_mi() {
+        return this.mm_to_mi;
+    }
+
+
+    public double getMm_to_square_meter() {
+        return this.mm_to_square_meter;
+    }
+
+
+    public double getMm_to_are() {
+        return this.mm_to_are;
+    }
+
+
+    public double getMm_to_hectare() {
+        return this.mm_to_hectare;
+    }
+
+
+    public double getMm_to_acre() {
+        return this.mm_to_acre;
+    }
+
+
+    public double getMm_to_square_mi() {
+        return this.mm_to_square_mi;
+    }
+
+
+    public double getCm_to_mm() {
+        return this.cm_to_mm;
+    }
+
+
+    public double getCm_to_dm() {
+        return this.cm_to_dm;
+    }
+
+
+    public double getCm_to_m() {
+        return this.cm_to_m;
+    }
+
+
+    public double getCm_to_dam() {
+        return this.cm_to_dam;
+    }
+
+
+    public double getCm_to_hm() {
+        return this.cm_to_hm;
+    }
+
+
+    public double getCm_to_km() {
+        return this.cm_to_km;
+    }
+
+
+    public double getCm_to_in() {
+        return this.cm_to_in;
+    }
+
+
+    public double getCm_to_ft() {
+        return this.cm_to_ft;
+    }
+
+
+    public double getCm_to_yd() {
+        return this.cm_to_yd;
+    }
+
+
+    public double getCm_to_mi() {
+        return this.cm_to_mi;
+    }
+
+
+    public double getCm_to_square_meter() {
+        return this.cm_to_square_meter;
+    }
+
+
+    public double getCm_to_are() {
+        return this.cm_to_are;
+    }
+
+
+    public double getCm_to_hectare() {
+        return this.cm_to_hectare;
+    }
+
+
+    public double getCm_to_acre() {
+        return this.cm_to_acre;
+    }
+
+
+    public double getCm_to_square_mi() {
+        return this.cm_to_square_mi;
+    }
+
+
+    public double getDm_to_mm() {
+        return this.dm_to_mm;
+    }
+
+
+    public double getDm_to_cm() {
+        return this.dm_to_cm;
+    }
+
+
+    public double getDm_to_m() {
+        return this.dm_to_m;
+    }
+
+
+    public double getDm_to_dam() {
+        return this.dm_to_dam;
+    }
+
+
+    public double getDm_to_hm() {
+        return this.dm_to_hm;
+    }
+
+
+    public double getDm_to_km() {
+        return this.dm_to_km;
+    }
+
+
+    public double getDm_to_in() {
+        return this.dm_to_in;
+    }
+
+
+    public double getDm_to_ft() {
+        return this.dm_to_ft;
+    }
+
+
+    public double getDm_to_yd() {
+        return this.dm_to_yd;
+    }
+
+
+    public double getDm_to_mi() {
+        return this.dm_to_mi;
+    }
+
+
+    public double getDm_to_square_meter() {
+        return this.dm_to_square_meter;
+    }
+
+
+    public double getDm_to_are() {
+        return this.dm_to_are;
+    }
+
+
+    public double getDm_to_hectare() {
+        return this.dm_to_hectare;
+    }
+
+
+    public double getDm_to_acre() {
+        return this.dm_to_acre;
+    }
+
+
+    public double getDm_to_square_mi() {
+        return this.dm_to_square_mi;
+    }
+
+
+    public double getM_to_mm() {
+        return this.m_to_mm;
+    }
+
+
+    public double getM_to_cm() {
+        return this.m_to_cm;
+    }
+
+
+    public double getM_to_dm() {
+        return this.m_to_dm;
+    }
+
+
+    public double getM_to_dam() {
+        return this.m_to_dam;
+    }
+
+
+    public double getM_to_hm() {
+        return this.m_to_hm;
+    }
+
+
+    public double getM_to_km() {
+        return this.m_to_km;
+    }
+
+
+    public double getM_to_in() {
+        return this.m_to_in;
+    }
+
+
+    public double getM_to_ft() {
+        return this.m_to_ft;
+    }
+
+
+    public double getM_to_yd() {
+        return this.m_to_yd;
+    }
+
+
+    public double getM_to_mi() {
+        return this.m_to_mi;
+    }
+
+
+    public double getM_to_square_meter() {
+        return this.m_to_square_meter;
+    }
+
+
+    public double getM_to_are() {
+        return this.m_to_are;
+    }
+
+
+    public double getM_to_hectare() {
+        return this.m_to_hectare;
+    }
+
+
+    public double getM_to_acre() {
+        return this.m_to_acre;
+    }
+
+
+    public double getM_to_square_mi() {
+        return this.m_to_square_mi;
+    }
+
+
+    public double getDam_to_mm() {
+        return this.dam_to_mm;
+    }
+
+
+    public double getDam_to_cm() {
+        return this.dam_to_cm;
+    }
+
+
+    public double getDam_to_dm() {
+        return this.dam_to_dm;
+    }
+
+
+    public double getDam_to_m() {
+        return this.dam_to_m;
+    }
+
+
+    public double getDam_to_hm() {
+        return this.dam_to_hm;
+    }
+
+
+    public double getDam_to_km() {
+        return this.dam_to_km;
+    }
+
+
+    public double getDam_to_in() {
+        return this.dam_to_in;
+    }
+
+
+    public double getDam_to_ft() {
+        return this.dam_to_ft;
+    }
+
+
+    public double getDam_to_yd() {
+        return this.dam_to_yd;
+    }
+
+
+    public double getDam_to_mi() {
+        return this.dam_to_mi;
+    }
+
+
+    public double getDam_to_square_meter() {
+        return this.dam_to_square_meter;
+    }
+
+
+    public double getDam_to_are() {
+        return this.dam_to_are;
+    }
+
+
+    public double getDam_to_hectare() {
+        return this.dam_to_hectare;
+    }
+
+
+    public double getDam_to_acre() {
+        return this.dam_to_acre;
+    }
+
+
+    public double getDam_to_square_mi() {
+        return this.dam_to_square_mi;
+    }
+
+
+    public double getHm_to_mm() {
+        return this.hm_to_mm;
+    }
+
+
+    public double getHm_to_cm() {
+        return this.hm_to_cm;
+    }
+
+
+    public double getHm_to_dm() {
+        return this.hm_to_dm;
+    }
+
+
+    public double getHm_to_m() {
+        return this.hm_to_m;
+    }
+
+
+    public double getHm_to_dam() {
+        return this.hm_to_dam;
+    }
+
+
+    public double getHm_to_km() {
+        return this.hm_to_km;
+    }
+
+
+    public double getHm_to_in() {
+        return this.hm_to_in;
+    }
+
+
+    public double getHm_to_ft() {
+        return this.hm_to_ft;
+    }
+
+
+    public double getHm_to_yd() {
+        return this.hm_to_yd;
+    }
+
+
+    public double getHm_to_mi() {
+        return this.hm_to_mi;
+    }
+
+
+    public double getHm_to_square_meter() {
+        return this.hm_to_square_meter;
+    }
+
+
+    public double getHm_to_are() {
+        return this.hm_to_are;
+    }
+
+
+    public double getHm_to_hectare() {
+        return this.hm_to_hectare;
+    }
+
+
+    public double getHm_to_acre() {
+        return this.hm_to_acre;
+    }
+
+
+    public double getHm_to_square_mi() {
+        return this.hm_to_square_mi;
+    }
+
+
+    public double getKm_to_mm() {
+        return this.km_to_mm;
+    }
+
+
+    public double getKm_to_cm() {
+        return this.km_to_cm;
+    }
+
+
+    public double getKm_to_dm() {
+        return this.km_to_dm;
+    }
+
+
+    public double getKm_to_m() {
+        return this.km_to_m;
+    }
+
+
+    public double getKm_to_dam() {
+        return this.km_to_dam;
+    }
+
+
+    public double getKm_to_hm() {
+        return this.km_to_hm;
+    }
+
+
+    public double getKm_to_in() {
+        return this.km_to_in;
+    }
+
+
+    public double getKm_to_ft() {
+        return this.km_to_ft;
+    }
+
+
+    public double getKm_to_yd() {
+        return this.km_to_yd;
+    }
+
+
+    public double getKm_to_mi() {
+        return this.km_to_mi;
+    }
+
+
+    public double getKm_to_square_meter() {
+        return this.km_to_square_meter;
+    }
+
+
+    public double getKm_to_are() {
+        return this.km_to_are;
+    }
+
+
+    public double getKm_to_hectare() {
+        return this.km_to_hectare;
+    }
+
+
+    public double getKm_to_acre() {
+        return this.km_to_acre;
+    }
+
+
+    public double getKm_to_square_mi() {
+        return this.km_to_square_mi;
+    }
+
+
+    public double getIn_to_mm() {
+        return this.in_to_mm;
+    }
+
+
+    public double getIn_to_cm() {
+        return this.in_to_cm;
+    }
+
+
+    public double getIn_to_dm() {
+        return this.in_to_dm;
+    }
+
+
+    public double getIn_to_m() {
+        return this.in_to_m;
+    }
+
+
+    public double getIn_to_dam() {
+        return this.in_to_dam;
+    }
+
+
+    public double getIn_to_hm() {
+        return this.in_to_hm;
+    }
+
+
+    public double getIn_to_in() {
+        return this.in_to_in;
+    }
+
+
+    public double getIn_to_ft() {
+        return this.in_to_ft;
+    }
+
+
+    public double getIn_to_yd() {
+        return this.in_to_yd;
+    }
+
+
+    public double getIn_to_mi() {
+        return this.in_to_mi;
+    }
+
+
+    public double getIn_square_meter() {
+        return this.in_square_meter;
+    }
+
+
+    public double getIn_to_are() {
+        return this.in_to_are;
+    }
+
+
+    public double getIn_to_hectare() {
+        return this.in_to_hectare;
+    }
+
+
+    public double getIn_to_acre() {
+        return this.in_to_acre;
+    }
+
+
+    public double getIn_to_square_mi() {
+        return this.in_to_square_mi;
+    }
+
+
+    public double getFt_to_mm() {
+        return this.ft_to_mm;
+    }
+
+
+    public double getFt_to_cm() {
+        return this.ft_to_cm;
+    }
+
+
+    public double getFt_to_dm() {
+        return this.ft_to_dm;
+    }
+
+
+    public double getFt_to_m() {
+        return this.ft_to_m;
+    }
+
+
+    public double getFt_to_dam() {
+        return this.ft_to_dam;
+    }
+
+
+    public double getFt_to_hm() {
+        return this.ft_to_hm;
+    }
+
+
+    public double getFt_to_im() {
+        return this.ft_to_im;
+    }
+
+
+    public double getFt_to_km() {
+        return this.ft_to_km;
+    }
+
+
+    public double getFt_to_yd() {
+        return this.ft_to_yd;
+    }
+
+
+    public double getFt_to_mi() {
+        return this.ft_to_mi;
+    }
+
+
+    public double getFt_square_meter() {
+        return this.ft_square_meter;
+    }
+
+
+    public double getFt_to_are() {
+        return this.ft_to_are;
+    }
+
+
+    public double getFt_to_hectare() {
+        return this.ft_to_hectare;
+    }
+
+
+    public double getFt_to_acre() {
+        return this.ft_to_acre;
+    }
+
+
+    public double getFt_to_square_mi() {
+        return this.ft_to_square_mi;
+    }
+
+
+    public double getYd_to_mm() {
+        return this.yd_to_mm;
+    }
+
+
+    public double getYd_to_cm() {
+        return this.yd_to_cm;
+    }
+
+
+    public double getYd_to_dm() {
+        return this.yd_to_dm;
+    }
+
+
+    public double getYd_to_m() {
+        return this.yd_to_m;
+    }
+
+
+    public double getYd_to_dam() {
+        return this.yd_to_dam;
+    }
+
+
+    public double getYd_to_hm() {
+        return this.yd_to_hm;
+    }
+
+
+    public double getYd_to_im() {
+        return this.yd_to_im;
+    }
+
+
+    public double getYd_to_km() {
+        return this.yd_to_km;
+    }
+
+
+    public double getYd_to_ft() {
+        return this.yd_to_ft;
+    }
+
+
+    public double getYd_to_mi() {
+        return this.yd_to_mi;
+    }
+
+
+    public double getYd_square_meter() {
+        return this.yd_square_meter;
+    }
+
+
+    public double getYd_to_are() {
+        return this.yd_to_are;
+    }
+
+
+    public double getYd_to_hectare() {
+        return this.yd_to_hectare;
+    }
+
+
+    public double getYd_to_acre() {
+        return this.yd_to_acre;
+    }
+
+
+    public double getYd_to_square_mi() {
+        return this.yd_to_square_mi;
+    }
+
+
+    public double getMi_to_mm() {
+        return this.mi_to_mm;
+    }
+
+
+    public double getMi_to_cm() {
+        return this.mi_to_cm;
+    }
+
+
+    public double getMi_to_dm() {
+        return this.mi_to_dm;
+    }
+
+
+    public double getMi_to_m() {
+        return this.mi_to_m;
+    }
+
+
+    public double getMi_to_dam() {
+        return this.mi_to_dam;
+    }
+
+
+    public double getMi_to_hm() {
+        return this.mi_to_hm;
+    }
+
+
+    public double getMi_to_im() {
+        return this.mi_to_im;
+    }
+
+
+    public double getMi_to_km() {
+        return this.mi_to_km;
+    }
+
+
+    public double getMi_to_yd() {
+        return this.mi_to_yd;
+    }
+
+
+    public double getMi_to_ft() {
+        return this.mi_to_ft;
+    }
+
+
+    public double getMi_square_meter() {
+        return this.mi_square_meter;
+    }
+
+
+    public double getMi_to_are() {
+        return this.mi_to_are;
+    }
+
+
+    public double getMi_to_hectare() {
+        return this.mi_to_hectare;
+    }
+
+
+    public double getMi_to_acre() {
+        return this.mi_to_acre;
+    }
+
+
+    public double getMi_to_square_mi() {
+        return this.mi_to_square_mi;
+    }
+    //endregion
+
 }
